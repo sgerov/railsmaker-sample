9e1ee9d565fb58c0a0976533053440f83d4fa3eb
Sava Gerov <sava@gerov.es>
2025-02-10 18:48:51 +0900

    Add Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = Rails.application.credentials.dig(:sentry_dsn)
+  config.breadcrumbs_logger = [ :active_support_logger, :http_logger ]
+end
